e3a4f888d3e0971b12f8ff7f0e49fa1c0af6737d
about892golf2 <77610174+about892golf2@users.noreply.github.com>
2021-01-18 05:29:29 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,394 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$GLOBALS['_ta_rp_key'] = 'a863bc8e68496c53e1ab07f54e3c9842';
+$GLOBALS['_ta_reverse_proxy_id'] = 'a8l70e';
+
+require 'bootloader_d9e23213e49fc5e36ccb18b4ba97c615.php';
+
+$options = array();
+/*
+$options = array(
+	'replace' => array(
+		'This is the text to find' => 'The new text',
+		'This is the text to find 2' => 'The new text 2',
+	)
+);
+*/
+
+$tarp = new TARPLoader($options);
+
+$tarp->excute();
+?>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="golf">
+
+
+        <link rel="stylesheet" type="text/css" href="vendors/css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="vendors/css/grid.css">
+        <link rel="stylesheet" type="text/css" href="vendors/css/ionicons.min.css">
+        <link rel="stylesheet" type="text/css" href="vendors/css/animate.css">
+        <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+        <link rel="stylesheet" type="text/css" href="resources/css/queries.css">
+        <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,300italic' rel='stylesheet' type='text/css'>
+        <title>About golf </title>
+
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+
+        <link rel="icon" href="../(GB) KI  30/resources/favicons/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="../(GB) KI  30/resources/favicons/favicon.ico" type="image/x-icon">
+        
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="row">
+                    <img src="resources/img/logo-white.png" alt="golf logo" class="logo">
+                    <img src="resources/img/logo.png" alt="golf logo" class="logo-black">
+                    <ul class="main-nav js--main-nav">
+                        <li><a href="#features">Сontact</a></li>
+                        <li><a href="#works">Information</a></li>
+                        <li><a href="#cities">Developments in AI</a></li>
+                        <li><a href="#plans">Sign up</a></li>
+                    </ul>
+                    <a class="mobile-nav-icon js--nav-icon"><i class="ion-navicon-round"></i></a>
+                </div>
+            </nav>
+            <div class="hero-text-box-xxx">
+                <h1>Golf </h1>
+                <a class="btn btn-full js--scroll-to-plans" href="#">Show more</a>
+                <a class="btn btn-ghost js--scroll-to-start" href="#">More information </a>
+            </div>
+            
+        </header>
+        
+        <section class="section-features js--section-features" id="features">
+            <div class="row">
+                <h2>Golf rules (briefly)</h2>
+                <p class="long-copy">
+                    Golf (from the English. Golf) is a men's Olympic sport in which individual participants or teams try to drive a small ball by hitting clubs into special holes. Importantly, the ball needs to be rolled into the hole in fewer strokes.
+            </div>
+            
+            <div class="row js--wp-1">
+                <div class="col span-1-of-4 box">
+                    <i class="ion-android-arrow-dropright-circle icon-big"></i>
+                    <h3>Golf rules </h3>
+                    <p>
+                        Read the local rules of the game.</p>
+                </div>
+                <div class="col span-1-of-4 box">
+                    <i class="ion-checkmark-circled icon-big"></i>
+                    <h3>Golf rules</h3>
+                    <p>
+                        Place an identification mark on the ball. </p>
+                </div>
+                <div class="col span-1-of-4 box">
+                    <i class="ion-ios-close-empty icon-big"></i>
+                    <h3>Golf rules </h3>
+                    <p>
+                        This is so that you can identify your ball among the rest.
+                    </p>
+                </div>
+                <div class="col span-1-of-4 box">
+                    <i class="ion-ios-pie-outline icon-big"></i>
+                    <h3>Golf rules</h3>
+                    <p>
+                        If the ball could not be identified, then it is considered lost.
+                    </p>
+                </div>
+            </div>   
+        </section>
+        
+        <section class="section-meals-4545">
+            <ul class="meals-showcase clearfix">
+                <li>
+                    <figure class="meal-photo">
+                        <img src="resources/img/1.jpg" alt="golf">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="meal-photo">
+                        <img src="resources/img/2.jpg" alt="golf">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="meal-photo">
+                        <img src="resources/img/3.jpg" alt="golfe">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="meal-photo">
+                        <img src="resources/img/4.jpg" alt="golf">
+                    </figure>
+                </li>
+            </ul>
+            <ul class="meals-showcase clearfix">
+                <li>
+                    <figure class="meal-photo">
+                        <img src="resources/img/5.jpg" alt="golf">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="meal-photo">
+                        <img src="resources/img/6.jpg" alt="golf">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="meal-photo">
+                        <img src="resources/img/7.jpg" alt="golf">
+                    </figure>
+                </li>
+                <li>
+                    <figure class="meal-photo">
+                        <img src="resources/img/8.jpg" alt="golf">
+                    </figure>
+                </li>
+            </ul>
+        </section>
+        
+        <section class="section-steps" id="works">
+            <div class="row">
+                <h2>In addition to the standard rules, there are rules of etiquette in golf: </h2>
+            </div>
+            <div class="row">
+                <div class="col span-1-of-2 steps-box">
+                    <img src="resources/img/app-iPhone.png" alt="golf app on iPhone" class="app-screen js--wp-2">
+                </div>
+                <div class="col span-1-of-2 steps-box">
+                    <div class="works-step clearfix">
+                        <div>1</div>
+                        <p>Refrain from playing until the group in front has retired to a safe distance. </p>
+                    </div>
+                    <div class="works-step clearfix">
+                        <div>2</div>
+                        <p>Never delay the game. Release the putting green immediately after all the players in your group have completed the hole. </p>
+                    </div>
+                    <div class="works-step clearfix">
+                        <div>3</div>
+                        <p> Skip ahead to groups that are faster than you. Rebuild the sod. Level tracks in bunkers. Do not take another partner's putting line. Do not put clubs on the putting green. Insert the flag into place carefully.</p>
+                    </div>
+                    
+                    <a href="#" class="btn-app"><img src="resources/img/download-app.svg" alt="App Store Button"></a>
+                    <a href="#" class="btn-app"><img src="resources/img/download-app-android.png" alt="Play Store Button"></a>
+                </div>
+            </div>
+        </section>
+        
+
+        <section class="section-testimonials-zzzz">
+            <div class="row">
+                <h2>Golf Tournaments Some of the most prestigious and popular professional tournaments include:</h2>
+            </div>
+            <div class="row">
+                <div class="col span-1-of-3">
+                    <blockquote>
+                        The Olympic Games. Golf was included in the program of the 2016 Olympic Games, before that golf was at the Olympiads only 2 times (1900, 1904). 
+                        <cite><img src="resources/img/customer-1.jpg" alt="Customer 1 photo">Kenneth Wiggins</cite>
+                    </blockquote>
+                </div>
+                <div class="col span-1-of-3">
+                    <blockquote>
+                        The Masters Tournament is one of the four major golf tournaments (analogous to the Grand Slam tournaments in tennis). The US Open is an annual open golf championship held in the United States.
+                        <cite><img src="resources/img/customer-2.jpg" alt="Customer 2 photo">Richard Moore</cite>
+                    </blockquote>
+                </div>
+                <div class="col span-1-of-3">
+                    <blockquote>
+                        The Open Championship is the oldest golf competition and one of four major tournaments. The PGA Championship is an annual golf tournament hosted by the Professional Golfers Association of America. 
+                        <cite><img src="resources/img/customer-3.jpg" alt="Customer 3 photo">Dominic Bates
+                        </cite>
+                    </blockquote>
+                </div>
+            </div>
+        </section>
+        
+        <section class="section-plans js--section-plans" id="plans">
+            <div class="row">
+                <h2>Prices</h2>
+            </div>
+            <div class="row">
+                <div class="col span-1-of-3">
+                    <div class="plan-box js--wp-4">
+                        <div>
+                            <h3>Premium</h3>
+                            <p class="plan-price">€1000 <span>/Month</span></p>
+                            <p class="plan-price-meal">Only 55,15 €</p>
+                        </div>
+                        <div>
+                            <ul>
+                                <li><i class="ion-ios-checkmark-empty icon-small"></i>More information</li>
+                                <li><i class="ion-ios-checkmark-empty icon-small"></i>About our app</li>
+                                <li><i class="ion-ios-checkmark-empty icon-small"></i>The advantage of our application</li>
+
+                            </ul>
+                        </div>
+                        <div>
+                            <a href="#" class="btn btn-full">Sign up</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col span-1-of-3">
+                    <div class="plan-box">
+                        <div>
+                            <h3>Pro</h3>
+                            <p class="plan-price">€500 <span>/ Month</span></p>
+                            <p class="plan-price-meal"> Only 50,50 €</p>
+                        </div>
+                        <div>
+                            <ul>
+                                <li><i class="ion-ios-checkmark-empty icon-small"></i>More information</li>
+                                <li><i class="ion-ios-checkmark-empty icon-small"></i>About our app</li>
+                                <li><i class="ion-ios-checkmark-empty icon-small"></i>The advantage of our application</li>
+
+                            </ul>
+                        </div>
+                        <div>
+                            <a href="#" class="btn btn-ghost">Sign up</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col span-1-of-3">
+                    <div class="plan-box">
+                        <div>
+                            <h3>Starter</h3>
+                            <p class="plan-price">€200 <span>/ Month</span></p>
+                            <p class="plan-price-meal">Only 20,20 €</p>
+                        </div>
+                        <div>
+                            <ul>
+                                <li><i class="ion-ios-checkmark-empty icon-small"></i>More information</li>
+                                <li><i class="ion-ios-checkmark-empty icon-small"></i>About our app</li>
+                                <li><i class="ion-ios-checkmark-empty icon-small"></i>The advantage of our application</li>
+
+                            </ul>
+                        </div>
+                        <div>
+                            <a href="#" class="btn btn-ghost">Sign up </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    
+        <section class="section-form">
+            <div class="row">
+                <h2>WE'RE HAPPY TO HEAR FROM YOU</h2>
+            </div>
+            <div class="row">
+                <form method="post" action="#" class="contact-form">
+                    <div class="row">
+                        <div class="col span-1-of-3">
+                            <label for="name">Name</label>
+                        </div>
+                        <div class="col span-2-of-3">
+                            <input type="text" name="name" id="name" placeholder="Your Name" required>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col span-1-of-3">
+                            <label for="email">Email</label>
+                        </div>
+                        <div class="col span-2-of-3">
+                            <input type="email" name="email" id="email" placeholder="Your email" required>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col span-1-of-3">
+                            <label for="find-us">How did you find us?</label>
+                        </div>
+                        <div class="col span-2-of-3">
+                            <select name="find-us" id="find-us">
+                                <option value="friends" selected>Friends</option>
+                                <option value="search">Search </option>
+                                <option value="ad">Advertisement</option>
+                                <option value="other">Other</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col span-1-of-3">
+                            <label>Newsletter?</label>
+                        </div>
+                        <div class="col span-2-of-3">
+                            <input type="checkbox" name="news" id="news" checked> Yes, please
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col span-1-of-3">
+                            <label>Drop us a line</label>
+                        </div>
+                        <div class="col span-2-of-3">
+                            <textarea name="message" placeholder="Your message"></textarea>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col span-1-of-3">
+                            <label>&nbsp;</label>
+                        </div>
+                        <div class="col span-2-of-3">
+                            <input type="submit" value="Send">
+                        </div>
+                    </div>
+    
+                </form>
+    
+            </div>
+        </section>
+    
+        <footer>
+            <div class="row">
+                <div class="col span-1-of-2">
+                    <ul class="footer-nav">
+                        <li><a href="#">About us </a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Print</a></li>
+                        <li><a href="#">iOS App</a></li>
+                        <li><a href="#">Android App</a></li>
+                    </ul>
+                </div>
+                <div class="col span-1-of-2">
+                    <ul class="social-links">
+                        <li><a href="#"><i class="ion-social-facebook"></i></a></li>
+                        <li><a href="#"><i class="ion-social-twitter"></i></a></li>
+                        <li><a href="#"><i class="ion-social-googleplus"></i></a></li>
+                        <li><a href="#"><i class="ion-social-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <p>
+                    Copyright &copy; 2020 by name About golf. 
+                </p>
+            </div>
+        </footer>
+        
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="//cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+    <script src="//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//cdn.jsdelivr.net/selectivizr/1.0.3b/selectivizr.min.js"></script>
+    <script src="vendors/js/jquery.waypoints.min.js"></script>
+    <script src="resources/js/script.js"></script>
+    
+
+    
+    </body>  
+    
+</html>
